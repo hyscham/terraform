68ab8195f55bc8bb6eaedee4b77f48305bfd8062
Hysham D2J <48835814+hyscham@users.noreply.github.com>
2020-07-16 21:15:51 +0100

    Update deploy3.sh

--- deploy3.sh
+++ deploy3.sh
@@ -1,149 +1,95 @@
+echo '*********************** Centos 7.5 *************************'
+echo 'Deploying ElasticSearch and Kibana on Cloud VM..............'
+echo '**************************** By h.ennakouch@gmail.com ******'
 
-provider "azurerm" {
-  #version= "=2.17.0"
-  features {}
-}
-
-#------------------------- Resouorce Group ------------------
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = var.location
-}
-
-#---------------------------Vnet-----------------------------
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-#----------------------- Subnet ----------------------------
-resource "azurerm_subnet" "internal" {
-  name                 = "${var.prefix}-subinternal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-#--------------------------NSG------------------------------
-resource "azurerm_network_security_group" "nsg" {
-    name = "${var.prefix}-nsg"
-    location = var.location
-    resource_group_name = azurerm_resource_group.main.name
-    
-    security_rule {
-      name = "SSH"
-      priority = 1001
-      direction = "Inbound"
-      access = "Allow"
-      protocol = "Tcp"
-      source_port_range = "*"
-      destination_port_range = "22"
-      source_address_prefix =  "*"
-      destination_address_prefix = "*"
-
-    }
-    security_rule {
-      name = "Elastic"
-      priority = 1002
-      direction = "Inbound"
-      access = "Allow"
-      protocol = "Tcp"
-      source_port_range = "*"
-      destination_port_range = "9200"
-      source_address_prefix =  "*"
-      destination_address_prefix = "*"
-
-    }
-    security_rule {
-      name = "kibana"
-      priority = 1003
-      direction = "Inbound"
-      access = "Allow"
-      protocol = "Tcp"
-      source_port_range = "*"
-      destination_port_range = "5601"
-      source_address_prefix =  "*"
-      destination_address_prefix = "*"
-
-    }
-
-}
-
-#-----------------------------Public IP ----------------------
-  resource "azurerm_public_ip" "pubip" {
-    resource_group_name = azurerm_resource_group.main.name
-    name = "${var.prefix}-public_ip"
-    location = var.location
-    allocation_method = "Dynamic"
-  }
-
-#------------------------------Network interface -------------
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-
-  ip_configuration {
-    name                          = "${var.prefix}-ipconfig"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Static"
-    private_ip_address = "10.0.2.100"
-    public_ip_address_id = azurerm_public_ip.pubip.id
-  }
-}
-#------------------------Assign NSG to nic ------------------------------------
-resource "azurerm_network_interface_security_group_association"  "nsg-to-nic" {
-    network_interface_id = azurerm_network_interface.main.id
-    network_security_group_id = azurerm_network_security_group.nsg.id
-
-  }
+echo '************************************************************'
+echo '********************* JDK configuration ********************'
+echo '************************************************************'
 
 
-# ----------------------------- Linux Centos VM --------------------------------
-resource "azurerm_linux_virtual_machine" "centosvm" {
-  name                            = "${var.prefix}-centos-vm"
-  computer_name                   = var.computer_name
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = var.location
-  size                            = "Standard_DS1_v2"
-  admin_username                  = var.admin_username
-  admin_password                  = var.admin_password
-  disable_password_authentication = false
-  network_interface_ids = [azurerm_network_interface.main.id]
+echo '1- Installing OpenJdk 1.8 ...'
+#sudo yum install java-1.8.0-openjdk -y
+sudo yum install java-1.8.0-openjdk-devel unzip -y
+echo '.....done'
 
-  
-    os_disk {
-      name = "${var.prefix}-vm_OsDisk"
-      caching = "ReadWrite"
-      storage_account_type = "Premium_LRS"
-    }
+echo '************************************************************'
+echo '********** Elastic Search 7.7.0 OSS Install ****************'
+echo '************************************************************'
 
-    source_image_reference {
-      publisher = "OpenLogic"
-      offer =  "CentOS"
-      sku = "7.5"
-      version = "latest"
-    }
-
-}
-# ----------------------------- CustomScript --------------------------------
-
-resource "azurerm_virtual_machine_extension" "vm-install-softs" {
-  name                 = var.computer_name
-  virtual_machine_id   = azurerm_linux_virtual_machine.centosvm.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-    {
-      
-        "fileUris": ["https://raw.githubusercontent.com/hyscham/terraform/master/deploy2.sh"],
-        "commandToExecute": "sh deploy2.sh"
-    }
-SETTINGS
-}
+echo '1- Move to new home dir'
+echo '.... done'
 
 
+echo '2- Create a working dir'
+echo '.... done'
+mkdir ~/efk
+
+cd efk
 
 
+echo '3- Download ElasticSearch 7.7.0 OSS'
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.7.0-linux-x86_64.tar.gz
+echo '.... done'
+
+echo '4- Untar downloaded package....'
+tar -xzf elasticsearch-oss-7.7.0-linux-x86_64.tar.gz
+mv elasticsearch*/ elasticsearch
+cd elasticsearch
+
+sudo ln -s /usr/lib/jvm/java-1.8.0/lib/tools.jar lib/
+
+echo "`ls -lha`"
+
+echo '5- Download and Install OpenDistro JOB SCHEDULER plugin ...'
+sudo bin/elasticsearch-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-job-scheduler/opendistro-job-scheduler-1.8.0.0.zip
+echo '..... Done'
+
+echo '6- Download and Install OpenDistro Alerting plugin for ElasticSearch ... '
+sudo bin/elasticsearch-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-alerting/opendistro_alerting-1.8.0.0.zip
+echo '... Done.'
+
+
+echo '6- Configuring network-host parameter for elasticsearch '
+echo 'network.host: 127.0.0.1' >> config/elasticsearch.yaml
+echo '... Done'
+
+echo '7- First run .... port 9200 should be opened on nsg '
+cd bin
+./elasticsearch
+
+echo '*****************************************************************'
+echo '**************** Kibana 7.7.0 OSS install  **********************'
+echo '*****************************************************************'
+
+echo '1- Download Kibana 7.7.0 OSS package'
+cd ~/efk
+sudo curl -O https://artifacts.elastic.co/downloads/kibana/kibana-oss-7.7.0-linux-x86_64.tar.gz
+echo '... done'
+
+echo '2- untar Kibana downloaded package'
+sudo tar -xzf kibana-oss-7.7.0-linux-x86_64.tar.gz
+echo '... done'
+
+echo '3- Rename Kibana folder'
+sudo mv kibana*/ kibana
+
+echo '4- Move to elastic Folder'
+cd kibana
+
+echo '5- Install OpenDistro Alerting plugin for Kibana'
+sudo bin/kibana-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/kibana-plugins/opendistro-alerting/opendistro-alerting-1.8.0.0.zip
+
+echo '6- Config Kibana parameters'
+echo 'server.host: 0.0.0.0' >> config/kibana.yml
+echo 'elasticsearch.hosts: ["http://localhost:9200"]' >> config/kibana.yml
+echo '.... done'
+
+
+echo '7- First run .... port 5601 should be opened on nsg '
+cd bin
+./kibana
+
+
+echo '*******************************************************************************************'
+echo ' CAP GEMINI '
+echo '**************************************   All rights reserved by Capgemini. Copyright © 2020'
