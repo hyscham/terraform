13a557f31250b60a9a5e5bbdec2f77bcb847dc0b
Hysham D2J <48835814+hyscham@users.noreply.github.com>
2020-07-18 18:38:00 +0100

    Update deploy3.sh

--- deploy3.sh
+++ deploy3.sh
@@ -2,17 +2,12 @@ echo '*********************** Centos 7.5 *************************'
 echo 'Deploying ElasticSearch and Kibana on Cloud VM..............'
 echo '**************************** By h.ennakouch@gmail.com ******'
 
-
-
 echo '*********************  Create efk user  ********************'
 useradd -m efk
-
-
 
 echo '************************************************************'
 echo '********************* JDK configuration ********************'
 echo '************************************************************'
-
 
 echo '1- Installing OpenJdk 1.8 ...'
 yum install java-1.8.0-openjdk-devel unzip -y
@@ -33,72 +28,65 @@ cd /home/efk/deploy
 echo '3- Download ElasticSearch 7.7.0 OSS'
 curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.7.0-linux-x86_64.tar.gz
 echo '.... done'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '4- Untar downloaded package....'
 tar -xzf elasticsearch-oss-7.7.0-linux-x86_64.tar.gz
 mv elasticsearch*/ elasticsearch
-
-
 cd elasticsearch
-
 sudo ln -s /usr/lib/jvm/java-1.8.0/lib/tools.jar lib/
-
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '5- Download and Install OpenDistro JOB SCHEDULER plugin ...'
 bin/elasticsearch-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-job-scheduler/opendistro-job-scheduler-1.8.0.0.zip
 echo '..... Done'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '6- Download and Install OpenDistro Alerting plugin for ElasticSearch ... '
 bin/elasticsearch-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/elasticsearch-plugins/opendistro-alerting/opendistro_alerting-1.8.0.0.zip
 echo '... Done.'
-
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '6- Configuring network-host parameter for elasticsearch '
-echo 'network.host: 0.0.0.0' >> config/elasticsearch.yml
-echo 'network.bind_host: 0.0.0.0' >> config/elasticsearch.yml
-echo 'node.master: true' >> config/elasticsearch.yml
-echo 'node.data: true' >> config/elasticsearch.yml
-echo 'transport.host: localhost' >> config/elasticsearch.yml
-
+#echo 'network.host: 0.0.0.0' >> config/elasticsearch.yml
+#echo 'network.bind_host: 0.0.0.0' >> config/elasticsearch.yml
+#echo 'node.master: true' >> config/elasticsearch.yml
+#echo 'node.data: true' >> config/elasticsearch.yml
+#echo 'transport.host: localhost' >> config/elasticsearch.yml
+echo 'cluster.name: HYS-AMI-OSS' >> config/elasticsearch.yml
+echo 'network.host: 127.0.0.1' >> config/elasticsearch.yml
+echo 'http.host: 0.0.0.0' >> config/elasticsearch.yml
 echo '... Done'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '7- First run .... port 9200 should be opened on nsg '
 chown -R efk:efk /home/efk/deploy/elasticsearch
-
-
-
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '*****************************************************************'
 echo '**************** Kibana 7.7.0 OSS install  **********************'
 echo '*****************************************************************'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '1- Download Kibana 7.7.0 OSS package'
 cd /home/efk/deploy
 curl -O https://artifacts.elastic.co/downloads/kibana/kibana-oss-7.7.0-linux-x86_64.tar.gz
 echo '... done'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '2- untar Kibana downloaded package'
 tar -xzf kibana-oss-7.7.0-linux-x86_64.tar.gz
 echo '... done'
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '3- Rename Kibana folder'
 mv kibana*/ kibana
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '4- Move to kibana Folder'
 cd kibana
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '5- Install OpenDistro Alerting plugin for Kibana'
 bin/kibana-plugin install --batch https://d3g5vo6xdbdb9a.cloudfront.net/downloads/kibana-plugins/opendistro-alerting/opendistro-alerting-1.8.0.0.zip
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '6- Config Kibana parameters'
 echo 'server.host: 0.0.0.0' >> config/kibana.yml
 echo 'elasticsearch.hosts: ["http://localhost:9200"]' >> config/kibana.yml
 echo '.... done'
-
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 echo '7- First run .... port 5601 should be opened on nsg '
 chown -R efk:efk /home/efk/deploy/kibana
-
+#------------------------------------------------------------------------------------------------------------------------------------------------------
 
 echo '*************************** Start Elastic ***************************************'
 cd /home/efk/deploy/elasticsearch/bin
@@ -107,13 +95,11 @@ echo '*************************** Start Kibana *********************************
 cd /home/efk/deploy/kibana/bin
 su efk -c "./kibana"
 
-
 echo '**********************************`Server_IP`***********************************************'
 echo '********************************************************************************************'
 ip a|grep inet
 echo '********************************************************************************************'
 echo '********************************************************************************************'
-
 
 
 echo '**********************************`Server_IP`***********************************************'
