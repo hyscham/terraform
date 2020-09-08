41e0c694c66bbde2e0b970fa24249dc4075faa34
Hysham D2J <48835814+hyscham@users.noreply.github.com>
2020-09-08 12:59:35 +0100

    Update deploy-clt.sh

--- deploy-clt.sh
+++ deploy-clt.sh
@@ -1,5 +1,5 @@
 echo '*********************** Centos 7.5 *************************'
-echo 'Deploying ElasticSearch and Kibana on Cloud VM..............'
+echo 'Config rsyslog, td-agent et metricbeat on Cloud VM..........'
 echo '**************************** By h.ennakouch@gmail.com ******'
 
 
@@ -7,11 +7,12 @@ echo '**************************** By h.ennakouch@gmail.com ******'
 
 echo '**************  Create efk user without password  ***********'
 sudo useradd -m efk
+echo 'Done'
 echo '*************************************************************'
 
 
 
-echo '2- Create a working dir'
+echo '2- Create a working dir *************************************'
 mkdir /home/efk/deploy
 echo '.... done'
 
@@ -24,41 +25,41 @@ yum install java-1.8.0-openjdk-devel unzip -y
 echo '.....done'
 echo '************************************************************'
 
+echo '************************************************************'
+echo '******* Download and install MetricBeat 7.7.0 OSS **********'
+echo '************************************************************'
 
-
-
-
-
-echo '*****************************************************************'
-echo '******* Download and install MetricBeat 7.7.0 OSS ***************'
-echo '*****************************************************************'
-
-echo '1- Move to deplyment folder' 
+echo '1- Move to deplyment folder ********************************' 
 cd /home/efk/deploy
 
-echo '2- Download Metricbeat OSS package'
-curl -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-oss-7.7.0-linux-x86_64.tar.gz
 
-echo '2- untar metricbeat downloaded package'
+echo '2- Download Metricbeat OSS package *************************'
+curl -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-oss-7.7.0-linux-x86_64.tar.gz
+echo 'Done'
+
+echo '3- untar metricbeat downloaded package *********************'
 mv metric* metricbeat.tar.gz
 tar -xzf metricbeat.tar.gz
 mv metric*/ metricbeat
+echo 'Done'
+
 
 chown -R efk /home/efk/deploy/metricbeat
 cd /home/efk/deploy/metricbeat
 mv metricbeat.yml metricbeat.yml.orig
 
 
-echo '3- Load metricbeat config file from Github'
+echo '3- Load metricbeat config file from Github ******************'
 curl -O https://raw.githubusercontent.com/hyscham/terraform/master/metricbeat-clt.yml
+echo 'Done'
 
-echo '3-1 Rename metricbeat config file from Github'
+echo '3-1 Rename metricbeat config file from Github **************'
 mv metricbeat-clt.yml metricbeat.yml
+echo 'Done'
 
-
-echo '4- Change metricbeat folder permissions to efk user' 
+echo '4- Change metricbeat folder permissions to efk user*********' 
 chown -R efk /home/efk/deploy/metricbeat
-
+echo 'Done'
 
 echo '****************************  display server IP for outside tests ****************************'
 curl ifconfig.co
@@ -88,28 +89,23 @@ su efk -c "./metricbeat -e &"
 echo '*************  Connect to server IP for outside tests ******************************************'
 echo '************************************************************************************************'
 echo '************************************************************************************************'
+echo '************************************************************************************************'
+echo '************************************************************************************************'
 
-echo "http://`curl ifconfig.co`:5601"
-echo '************************************************************************************************'
-echo '************************************************************************************************'
-echo '************************************************************************************************'
-echo '************************************************************************************************'
 
 
 echo '************************************ Ssh config (Permit PasswordAuth) **************************'
 echo 'Permit ssh login with password after deployment to fine tune configuration'
 sudo sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sudo sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-
-PermitRootLogin
 sudo systemctl restart sshd
 echo '************************************************************************************************'
 
-
-
-
-
+echo '************************************************************************************************'
+echo '************************************************************************************************'
 echo '************************************************************************************************'
 echo ' CAP GEMINI '
 echo '**************************************   All rights reserved by Capgemini. Copyright © 2020 ****'
+echo '************************************************************************************************'
+echo '************************************************************************************************'
+echo '************************************************************************************************'
